c71563096985d50bbf959cefc508ad37d77d4efa
Prabin Pokharel <153236723+PrabinPokharel@users.noreply.github.com>
2023-12-08 21:06:50 +1100

    Update square.py

--- square.py
+++ square.py
@@ -1,7 +1,7 @@
 # Get the size of the square from the user
 n = int(input("Enter the size of the square: "))
 
-# Draw the square using the print function
+# Draw the square using the print function ????
 for i in range(n):
     for j in range(n):
         if i==0 or i==n-1 or j==0 or j==n-1:
