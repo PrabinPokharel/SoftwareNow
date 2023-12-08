5c51f24ae1689390a3f36aafd3bb912fc507d1cd
Pragyan Pandey <150564792+PragyanPP@users.noreply.github.com>
2023-12-08 19:30:53 +0930

    Add files via upload

---
+++ square.py
@@ -0,0 +1,11 @@
+# Get the size of the square from the user
+n = int(input("Enter the size of the square: "))
+
+# Draw the square using the print function
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1 or j==0 or j==n-1:
+         print("*",end=" ")
+        else:
+         print(" ", end=" ")
+    print("")  # Move to the next line after each row
